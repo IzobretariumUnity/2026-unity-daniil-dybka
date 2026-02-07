758b95555fdefce0d5384d1b9af6ef9d2f2707bf
Daniil Dybka <danil.dybko@gmail.com>
2026-02-07 12:36:44 +0300

    Добавляет PlayerHealthUI

--- Assets/Scripts/Player/PlayerHealthUI.cs
+++ Assets/Scripts/Player/PlayerHealthUI.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerHealthUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject prefabHealth;
+    public float space = 140f;
+    public bool moveRight = true;
+
+    private string healthParent = "ui_HealthBar";
+
+    private GameObject parent;
+    private List<GameObject> objectsHealth = new List<GameObject>();
+
+    private void Start()
     {
-        
+        ParentInit();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitUI(int currentHealth, int maxHealth)
     {
-        
+        for (int i = 0; i < maxHealth; i++)
+        {
+            if (parent == false) ParentInit();
+
+            GameObject initPrefabHealth = Instantiate(prefabHealth, parent.transform, false);
+
+            RectTransform rt = initPrefabHealth.GetComponent<RectTransform>();
+            Vector2 pos = rt.anchoredPosition;
+
+            float dir = moveRight ? 1f : -1f;
+            rt.anchoredPosition = new Vector2(pos.x + dir * i * space, pos.y);
+
+            objectsHealth.Add(initPrefabHealth);
+        }
+
+        UpdateUI(currentHealth);
+    }
+
+    public void UpdateUI(int health)
+    {
+        for (int i = 0; i < objectsHealth.Count; i++)
+        {
+            objectsHealth[i].SetActive(i < health);
+        }
+    }
+
+    private void ParentInit()
+    {
+        parent = GameObject.FindGameObjectWithTag(healthParent);
     }
 }
